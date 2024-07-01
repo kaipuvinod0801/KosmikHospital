502217bb09c09d38e9b032a2d6fafc112bc52e65
kaipuvinod0801 <kaipuvinod0801@gmail.com>
2024-07-01 10:28:32 +0530

    This is Project

---
+++ src/test/java/TestCaseExecution/ExecutionClass.java
@@ -0,0 +1,26 @@
+package TestCaseExecution;
+
+import java.awt.AWTException;
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import BasePack.BaseClass;
+
+public class ExecutionClass extends BaseClass {
+	@Test
+	public void execute() throws InterruptedException, IOException, AWTException {
+
+		ScriptCoding s = new ScriptCoding();
+		
+		 s.login();
+
+		 s.perminentRegistrationforoneperson();
+
+		 s.perminentRegistrationformultipleperson();
+
+		 s.editoneEmployees();
+
+		 s.editmultipleEmployees();
+	}
+}
